69e9f12902efa330175ec3a1d986583da9e32643
liuguanbang <salodyliu@gmail.com>
2017-10-14 23:32:23 +0800

    添加login模块的saga loginWatch逻辑

--- src/login/sagas.js
+++ src/login/sagas.js
@@ -1,28 +1,60 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { take, fork, cancel, call, put, cancelled } from 'redux-saga/effects';
+import { browserHistory } from 'react-router'
+import { handleApiErrors } from '../lib/api-error';
+
+// login constants
 import {
   LOGIN_REQUESTING,
   LOGIN_SUCCESS,
   LOGIN_ERROR
 } from "./constants";
-import { handleApiErrors } from '../lib/api-error';
+
+// So that we can modify our Client piece of state
+import {
+  setClinet,
+  unsetClinet
+} from "../client/actions";
+
+import {
+  CLIENT_SET,
+  CLIENT_UNSET
+} from "../client/constants";
 
 function loginApi() {
 
 }
 
+function* logout() {
 
-function* loginFlow (action) {
-  try {
-    const { email, password } = action;
-    const response = yield call(loginApi, email, password);
-    yield put({ type: LOGIN_SUCCESS, response })
-  } catch(error) {
-    yield put({ type: LOGIN_ERROR, error})
-  }
+}
+
+function* loginFlow (email, password) {
+
 }
 
 function* loginWatch () {
-  yield takeLatest(LOGIN_REQUESTING, loginFlow);
+  // Starting a loop.
+  while (true) {
+    // watching for the LOGIN_REQUESTING action
+    const { email, password } = yield take(LOGIN_REQUESTING);
+
+    // Fork a background task that will run loginFlow()。
+    // 这个是异步的 不会阻止下面代码的进行。
+    const task = yield fork(loginFlow, email, password);
+
+    // Begin watching for CLIENT_UNSET and LOGIN_ERROR
+    const action = yield take([CLIENT_UNSET, LOGIN_ERROR]);
+
+    //  If the action.type is CLIENT_UNSET cancel our forked task of trying to login
+    if (action.type === CLIENT_UNSET) yield cancel(task);
+
+    // We log out the by calling to our logout function
+    // Once we code that out, it will simply dispatch a CLIENT_UNSET action that will remove the data from our state, remove the token and redirect them to the login screen.
+    yield call(logout);
+
+    // once we've logged them back out.
+    // the loop begins again and starts watching for LOGIN_REQUESTING! Because while(true) is surprisingly true, we start right back at the top and begin again.
+  }
 }
 
 export default loginWatch;
